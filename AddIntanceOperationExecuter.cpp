07f966bac9006c019fc8d186aad79a4500e2f9a3
pengqiang <pengqiang1979@gmail.com>
2013-06-08 18:02:13 +0800

    AddInstanceOperation

---
+++ AddIntanceOperationExecuter.cpp
@@ -0,0 +1,12 @@
+#include "StdAfx.h"
+#include "AddIntanceOperationExecuter.h"
+
+
+AddIntanceOperationExecuter::AddIntanceOperationExecuter(void)
+{
+}
+
+
+AddIntanceOperationExecuter::~AddIntanceOperationExecuter(void)
+{
+}
