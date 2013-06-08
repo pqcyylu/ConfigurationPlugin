c1de6295affcbf2df0c9531b54d6f589daec5ce3
pengqiang <pengqiang1979@gmail.com>
2013-06-08 17:59:49 +0800

    AddOperationExecuter

---
+++ AddIntanceOperationExecuter.h
@@ -0,0 +1,30 @@
+//===========================================================================//
+//  FILE:  AddIntanceOperationExecuter.h											//
+//------------------------------------------------------------------------	//
+//	PURPOSE:																//
+//			实例添加操作执行器。												//
+//																			//
+//------------------------------------------------------------------------	//
+//  AUTHOR: datangmobile LMT-B 	pengqiang	    							//
+//			pengqiang@datangmobile.cn	TEL: +86 10 58833109.   			//
+//	Copyright:																//
+// Copyright (c) 2005-2015, DatangMobile Corporation.  All rights reserved.	//
+//	---------------------------------------------------------------------	//
+//	Note:                    												//
+//			                                         						//
+//			                                                            	//
+//-----------------------------------------------------------------------	//
+//	 HISTORY:																//
+//	 2013-06-07 - create by pengqiang										//
+//==========================================================================//
+#pragma once
+
+class AddIntanceOperationExecuter
+{
+public:
+	AddIntanceOperationExecuter(void);
+	virtual ~AddIntanceOperationExecuter(void);
+
+
+};
+
